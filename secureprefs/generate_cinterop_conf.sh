9d3228c924b831a821a82612ab956ac18431b39b
Vu Phan <ptvu.it@gmail.com>
2023-06-22 19:06:52 -0700

    fix: remove jcenter

--- secureprefs/generate_cinterop_conf.sh
+++ secureprefs/generate_cinterop_conf.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 current_dir=$1
-target_file_dir="$1/src/nativeinterop/cinterop/"
+target_file_dir="$1/src/nativeInterop/cinterop/"
 target_file_name="keychainwrapper.def"
 
 rm -f "$target_file_dir$target_file_name"
@@ -8,6 +8,7 @@ rm -f "$target_file_dir$target_file_name"
 echo "writing config to $target_file_dir$target_file_name"
 echo "currentdir $current_dir"
 mkdir -p $target_file_dir
+echo "$target_file_dir$target_file_name"
 echo \
 "language = Objective-C
 headers = Keychain.h KeychainQuery.h
@@ -15,4 +16,4 @@ compilerOpts = -Fsecureprefs/library/keychainwrapper/bin -fembed-bitcode -embed-
 linkerOpts = -Fsecureprefs/library/keychainwrapper/bin
 staticLibraries = keychainwrapper.a
 libraryPaths = secureprefs/library/keychainwrapper/bin" \
->> ".$target_file_dir$target_file_name"
+>> "$target_file_dir$target_file_name"
